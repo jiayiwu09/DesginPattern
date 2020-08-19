e37f390a3557fb471a7f39e808ba1763e143c2bf
XuanMclane <xuanmclane@gmail.com>
2020-08-18 22:55:24 -0400

    Chain of responsibility

---
+++ src/com/DesignPatterns/ChainOfResponsibility/Handler.java
@@ -0,0 +1,18 @@
+package com.DesignPatterns.ChainOfResponsibility;
+
+public abstract class Handler {
+    private Handler next;
+
+    public Handler(Handler next) {
+        this.next = next;
+    }
+
+    public abstract boolean doHandle(HttpRequest request);
+
+    public void handle(HttpRequest request) {
+        if (doHandle(request))
+            return;
+        if (next != null)
+            next.handle(request);
+    }
+}
